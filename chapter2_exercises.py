52b0261c4d39eda7d7d2499f01b4bc252703282f
Risa <selfrising@gmail.com>
2014-06-03 20:59:21 -0500

    the first week part 1

--- chapter2_exercises.py
+++ chapter2_exercises.py
@@ -1,3 +1,34 @@
 # Exercises for chapter 2:
 
-# Name:
+# Name: Lisa F 
+
+# Exercise 3
+"""	
+	3.1: width/2 = 8.5
+	3.2: width/2.0 = 8.5
+	3.3: height/3 = 4.0
+	3.4: 1 + 2 * 5 = 11
+	3.5: delimiter * 5 = .....
+"""
+
+# Exercise 4
+"""	
+	 4.1: 4/3 * 3.14 * 5 ** 3 = 523.33
+	 4.2: 
+		b =	24.95 - 24.95 * 0.4
+		s = 3 + 0.75 * n
+		n = 60
+
+		wholesale cost = 60 * b + n
+		wholesale cost = 946.20
+
+	 4.3: 
+	 	p1 = 8.25
+	 	p2 = 7.2
+
+	 	min = p1 * 2 + 3 * p2
+	 	min = 38.1
+
+	 	06:52:00 + 00:38:06 = 07:30:06
+"""
+
